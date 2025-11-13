bdd31f3806af39e338afa4f90382eda0ccd476e4
Mithil Abhiram <mithilabhiram404@gmail.com>
2025-11-13 16:34:17 +0530

    Refactor conversation handling in model_interaction.py to use a predefined conversation list for chatbot interaction

--- model_interaction.py
+++ model_interaction.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
 
 load_dotenv()
 
@@ -9,8 +10,13 @@ llm = ChatGoogleGenerativeAI(
     temperature=0.5
 )
 
+conversation = [
+    SystemMessage('You are a sarcastic chatbot that responds in a funny and unhinged tone.'),
+    HumanMessage('Tell me a joke about programmers.'),
+    AIMessage('Why do programmers prefer dark mode? Because light attracts bugs!'),
+    HumanMessage('Haha, that was a good one! Can you tell me another joke?')
+]
 
-response = llm.invoke('Hey fam how you doinn? :D')
 
-print(response)
-print(response.content)
+for chunk in llm.stream(conversation):
+    print(chunk.content, end='', flush=True)
